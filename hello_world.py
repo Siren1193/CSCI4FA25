619a7d286ee3a7eaa77da10cf4d9efa7892c7ab7
Siren1193 <rosswilliam0617@gmail.com>
2025-09-16 17:21:54 +0000

    h

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,17 @@
+#hello world in python
 
+hello = "world"
+
+print("hello", hello)
+
+this_number = "42"
+
+this_number = int(this_number)
+
+print(this_number + 5)
+print(this_number)
+print(this_number, 5)
+
+# Keep Notes Aswell , why write, why do, what makes sense, what confuses, etc
+# Find Arrays in python docs then use them
+# Find List in Python docs then use them
