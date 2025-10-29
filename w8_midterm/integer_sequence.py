fd0e2dd7cf90995167c49cb23a772d97aa11a553
Siren1193 <rosswilliam0617@gmail.com>
2025-10-29 02:11:50 +0000

    comments for integer sequence

--- w8_midterm/integer_sequence.py
+++ w8_midterm/integer_sequence.py
@@ -1,10 +1,10 @@
 #Triangular number sequence in python
 
-def aetherflux(n):
-    life = [0]
-    for life_gained in range(1, n + 1):
-        casts = (life_gained * (life_gained + 1)) // 2
-        life.append(casts)
-    return life
+life = []                                               # Creates the list so we can index the amount of life gained.
+def aetherflux(N):                                      # Our function to find the life gained by Aetherflux Reservoir.
+    for life_gained in range(0, N + 1):                 # Loops our equation through index 0 to index N.
+        casts = (life_gained * (life_gained + 1)) // 2  # Our equation for the amount of life gained from Aetherflux Reservoir
+        life.append(casts)                              # Appends our result (casts) to the end of the list (life) to continue the loop.
+    return life                                         # Ends our funtion, and writes our list.
 
-print(aetherflux(100))
+print(aetherflux(100))                                  # Prints the function aetherflux to index 100.
