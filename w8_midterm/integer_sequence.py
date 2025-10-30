3024a7f96b86661c2721ec3b3fb302c99af35a2a
Siren1193 <rosswilliam0617@gmail.com>
2025-10-30 16:28:02 +0000

    updated integer_sequence.py for mid term

--- w8_midterm/integer_sequence.py
+++ w8_midterm/integer_sequence.py
@@ -1,10 +1,12 @@
-#Triangular number sequence in python
+#Triangular number sequence in python using Aetherflux Reservior as a reference in naming
 
-life = []                                               # Creates the list so we can index the amount of life gained.
-def aetherflux(N):                                      # Our function to find the life gained by Aetherflux Reservoir.
-    for life_gained in range(0, N + 1):                 # Loops our equation through index 0 to index N.
-        casts = (life_gained * (life_gained + 1)) // 2  # Our equation for the amount of life gained from Aetherflux Reservoir
-        life.append(casts)                              # Appends our result (casts) to the end of the list (life) to continue the loop.
-    return life                                         # Ends our funtion, and writes our list.
+life = []                                                                       # Creates an empty list so we can index the amount of life gained. The index of the list corresponds to the number of spells cast.
+def aetherflux(N):                                                              # Names our function to find the life gained by Aetherflux Reservoir to the Nth value.
+    for spells_cast in range(0, N + 1):                                         # Loops our equation through each possible number of spells cast from 0 to N.
+        life_gained = (spells_cast * (spells_cast + 1)) // 2                    # Our equation for the amount of life gained from Aetherflux Reservoir.
+        life.append(life_gained)                                                # Appends our result (casts) to the end of the list (life) to continue the loop until the total amount requested has been appended.
+    return life                                                                 # Ends our funtion, and writes our list.
 
-print(aetherflux(100))                                  # Prints the function aetherflux to index 100.
+aetherflux(100)                                                                 # This fills the lists first 100 indexes so we can then call a specific one when we print our output message, though this method can be resource intensive.
+
+print("You've gained", life[3], "life from Aetherflux Reservoir this turn!")    # Output Message, changing the value for life[N] changes the index to the specific one we choose equal to the number of spells you cast.
